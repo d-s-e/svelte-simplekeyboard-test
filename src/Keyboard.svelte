fd78500a13f1f51e900e63e23bad67061eb1c8a3
Guenther Meyer <d.s.e@sordidmusic.com>
2020-10-28 10:15:36 +0100

    initial commit

---
+++ src/Keyboard.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Keyboard from 'simple-keyboard'
+
+    let keyboard = new Keyboard({
+        onChange: input => onChange(input),
+        onKeyPress: button => onKeyPress(button)
+    });
+
+    function onChange(input){
+        document.querySelector(".input").value = input;
+        console.log("Input changed", input);
+    }
+
+    function onKeyPress(button){
+        console.log("Button pressed", button);
+    }
+</script>
+
+
+<input class="input" type="text" value="foo bar"/>
+<div class="simple-keyboard"></div>
+
+
+<style>
+    @import 'simple-keyboard/build/css/index.css';
+</style>
+
